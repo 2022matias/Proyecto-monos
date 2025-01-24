f52fd4db3a8132275902deac1cbc923537f3f44c
javierdamiani <javierdamiani74@gmail.com>
2025-01-24 14:37:19 -0500

    proyecto copiado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				yellow: '#F2E901'
+			},
+			fontFamily: {
+				daciaBold: ['DaciaBlockExtendedBold', 'sans-serif'],
+				daciaLight: ['DaciaBlockExtendedLight', 'sans-serif'],
+				daciaExtended: ['DaciaBlockExtended', 'sans-serif'],
+				daciaRegular: ['DaciaBlock', 'sans-serif'],
+				daciaThin: ['DaciaBlockLight', 'sans-serif'],
+				inter: ['Inter', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
-}
+};
